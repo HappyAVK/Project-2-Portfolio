f7a3ae0fa651774e8af2e7265591c6ba0e578526
Alex <alexlsouthall@yahoo.com>
2022-12-13 22:18:47 +0000

    Added SubHeaders and information with pandas

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
@@ -14,3 +15,19 @@ with col2:
     st.info(content)
 
 st.subheader("If any of these apps interest you, feel free to contact me about them!")
+df = pandas.read_csv("data.csv", sep=";")
+col3, col4, col5= st.columns([1.5,0.5,1.5])
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
+
+with col5:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/"+row["image"])
+        st.write(f"[Source Code]({row['url']})")
