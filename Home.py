6454d6f85e0a8ca9b3271276ab8b2e9a63a2ee0a
Alex <alexlsouthall@yahoo.com>
2023-01-26 17:39:52 +0000

    Moved all content to one page - Removed unecessary files

--- Home.py
+++ Home.py
@@ -2,32 +2,64 @@ import streamlit as st
 import pandas
 
 st.set_page_config(layout="wide")
-col1, col2 = st.columns(2)
+col0, col1, col2 = st.columns(3)
+
 
 with col1:
-    st.image("images/drugs.jpg")
-
-with col2:
     st.title("Alexander Southall")
-    content= """I'm Alex, Im learning Python everyday, 
-    I Graduated from the University of Greenwich with a specialization in animation, 
-    but with python I aim to lean more into Software development, annd Data Science!"""
-    st.info(content)
 
-st.subheader("If any of these apps interest you, feel free to contact me about them!")
-df = pandas.read_csv("data.csv", sep=";")
-col3, col4, col5= st.columns([1.5,0.5,1.5])
+    st.image("images/thumbnail.png", width=300)
 
-with col3:
-    for index, row in df[:10].iterrows():
-        st.header(row["title"])
-        st.write(row["description"])
-        st.image("images/" + row["image"])
-        st.write(f"[Source Code]({row['url']})")
+    select_box = st.selectbox(label="Page Selection", options=["About", "Programming Projects", "Contact via Email"])
 
-with col5:
-    for index, row in df[10:].iterrows():
-        st.header(row["title"])
-        st.write(row["description"])
-        st.image("images/"+row["image"])
-        st.write(f"[Source Code]({row['url']})")
+
+
+match select_box:
+    case "Contact via Email":
+        from send_email import send_mail
+
+        st.header("Contact here")
+
+        st.subheader("Enter Your Email")
+
+        with st.form(key="e_form"):
+            user_email = st.text_input("Enter your Email")
+            raw_message = st.text_area("Your Message")
+            message = f"""\
+        Subject: New email from: {user_email}
+
+        From: {user_email} 
+        {raw_message}
+        """
+
+            button = st.form_submit_button("Send")
+            if button:
+                send_mail(message)
+                st.info("Message sent")
+    case "Programming Projects":
+
+        st.subheader("If any of these apps interest you, feel free to contact me about them!")
+        df = pandas.read_csv("new_data.csv", sep=";")
+        col3, col4, col5 = st.columns([1.5, 0.5, 1.5])
+
+        with col3:
+            for index, row in df[:10].iterrows():
+                if row["State"] == "Yes":
+                    st.header(row["title"])
+                    st.write(row["description"])
+                    st.image("images/" + row["image"], width=200)
+                    st.write(f"[Github Repo]({row['url']})")
+
+        with col5:
+            for index, row in df[10:].iterrows():
+                if row["State"]=="Yes":
+                    st.header(row["title"])
+                    st.write(row["description"])
+                    st.image("images/" + row["image"], width=200)
+                    st.write(f"[Github Repo]({row['url']})")
+    case "About":
+        col4, col5, col6 = st.columns(3)
+        with col5:
+            st.write("Hi Im Alexander Southall I am a 2021 Graduate from the University of Greenwich, here you will find some Python projects I've implemented to develop my skills, please take a look and I would love to hear some feedback, thanks!")
+
+
